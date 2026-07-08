5b27b8522c8e472025ae21dcf4be3886d2679dd5
Nowell222 <nowellandal71@gmail.com>
2026-07-08 14:59:54 +0800

    chore(web): update vite config options to support tailwind css and pwa plugins

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
-
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -34,4 +34,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
