da93935a06e897f65992674239df4aed5de6789d
nullture <ysaandre.mendoza@gmail.com>
2026-07-13 16:23:51 +0800

    chore: update branch with latest admin base changes

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,21 +1,21 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-import "./index.css";
-import App from "./App";
+import { QueryClient } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
 
-import { QueryClient } from "@tanstack/react-query";
-import { QueryClientProvider } from "@tanstack/react-query";
+import { registerSW } from "virtual:pwa-register";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
-createRoot(document.getElementById("root")!).render(
+registerSW();
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </QueryClientProvider>
   </StrictMode>
-);
+)
