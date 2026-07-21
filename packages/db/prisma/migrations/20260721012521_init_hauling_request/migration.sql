29ee5b491f9bb62cf55846f1596c5a99249e0953
nullture <ysaandre.mendoza@gmail.com>
2026-07-21 09:37:08 +0800

    chore(db): align database schema with latest development branch

--- packages/db/prisma/migrations/20260721012521_init_hauling_request/migration.sql
+++ packages/db/prisma/migrations/20260721012521_init_hauling_request/migration.sql
@@ -2,6 +2,7 @@
   Warnings:
 
   - You are about to drop the `ServiceRequest` table. If the table is not empty, all the data it contains will be lost.
+  - Made the column `password` on table `User` required. This step will fail if there are existing NULL values in that column.
 
 */
 -- CreateEnum
@@ -12,6 +13,18 @@ CREATE TYPE "WasteType" AS ENUM ('RESIDUAL', 'NON_BIODEGRADABLE', 'HAZARDOUS', '
 
 -- CreateEnum
 CREATE TYPE "SenderType" AS ENUM ('RESIDENT', 'BUSINESS');
+
+-- AlterTable
+ALTER TABLE "DriverLocation" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "truckId" TEXT;
+
+-- AlterTable
+ALTER TABLE "Truck" ALTER COLUMN "model" DROP NOT NULL,
+ALTER COLUMN "capacity" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "name" TEXT,
+ALTER COLUMN "password" SET NOT NULL;
 
 -- DropTable
 DROP TABLE "ServiceRequest";
@@ -49,3 +62,6 @@ CREATE TABLE "HaulingRequest" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "HaulingRequest_request_number_key" ON "HaulingRequest"("request_number");
+
+-- AddForeignKey
+ALTER TABLE "DriverLocation" ADD CONSTRAINT "DriverLocation_truckId_fkey" FOREIGN KEY ("truckId") REFERENCES "Truck"("id") ON DELETE SET NULL ON UPDATE CASCADE;
