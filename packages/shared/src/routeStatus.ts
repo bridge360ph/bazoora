0c719a98f245c9a7fb382498195a94b05951fd07
nullture <ysaandre.mendoza@gmail.com>
2026-07-29 17:27:43 +0800

    refactor(shared): reorganize shared type definitions

--- packages/shared/src/routeStatus.ts
+++ packages/shared/src/routeStatus.ts
@@ -1,3 +1,10 @@
+/**
+ * Status values that may be assigned to a Route.
+ *
+ * These values are shared between
+ * frontend, backend and websocket events.
+ */
+
 export type RouteStatus =
   | "In Progress"
   | "Completed"
